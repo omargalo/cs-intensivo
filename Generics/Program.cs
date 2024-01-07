fb5f85d49ef34ce4776003a15d18928a26c38c7a
Omar García López <omar.garcia@omargl.net>
2024-01-07 08:45:50 -0600

    Seralize Desaralize

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -80,7 +80,7 @@ public class MyList<T>
 
 public class Beer
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public decimal Price { get; set; }
 
     public override string ToString()
